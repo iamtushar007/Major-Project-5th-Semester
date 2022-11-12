a5db0aed54878191f317c9bb6808ee3f226c2f29
iamtushar007 <tushargahtori26@gmail.com>
2022-11-13 04:48:43 +0530

    13th November 2022 :: 4:58 a.m.

--- app/src/main/java/com/example/letschat/OTPActivity.java
+++ app/src/main/java/com/example/letschat/OTPActivity.java
@@ -1,14 +1,101 @@
 package com.example.letschat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.letschat.databinding.ActivityOtpactivityBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.mukesh.OnOtpCompletionListener;
+
+import java.util.concurrent.TimeUnit;
 
 public class OTPActivity extends AppCompatActivity {
+
+    ActivityOtpactivityBinding binding;
+    FirebaseAuth auth;
+
+    String verificationId;
+
+    ProgressDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_otpactivity);
+        binding=ActivityOtpactivityBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        dialog=new ProgressDialog(this);
+        dialog.setMessage("Sending OTP...");
+        dialog.setCancelable(false);
+        dialog.show();
+
+        auth=FirebaseAuth.getInstance();
+
+        getSupportActionBar().hide();
+
+        String phoneNumber=getIntent().getStringExtra("phoneNumber");
+
+        binding.phoneLabel.setText("Verify "+phoneNumber);
+
+        PhoneAuthOptions options=PhoneAuthOptions.newBuilder(auth)
+                .setPhoneNumber(phoneNumber)
+                .setTimeout(60L, TimeUnit.SECONDS)
+                .setActivity(OTPActivity.this)
+                .setCallbacks(new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                    @Override
+                    public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+
+                    }
+
+                    @Override
+                    public void onVerificationFailed(@NonNull FirebaseException e) {
+
+                    }
+
+                    @Override
+                    public void onCodeSent(@NonNull String verifyId, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                        super.onCodeSent(verifyId, forceResendingToken);
+
+                        dialog.dismiss();
+                        verificationId=verifyId;
+                    }
+                }).build();
+
+        PhoneAuthProvider.verifyPhoneNumber(options);
+
+        binding.otpView.setOtpCompletionListener(new OnOtpCompletionListener() {
+            @Override
+            public void onOtpCompleted(String otp) {
+                PhoneAuthCredential credential=PhoneAuthProvider.getCredential(verificationId,otp);
+
+                auth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful())
+                        {
+                            Toast.makeText(OTPActivity.this, "Logged In", Toast.LENGTH_SHORT).show();
+                        }else
+                        {
+                            Toast.makeText(OTPActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+
+
+
     }
 }
--- app/src/main/java/com/example/letschat/PhoneNumberActivity.java
+++ app/src/main/java/com/example/letschat/PhoneNumberActivity.java
@@ -2,13 +2,33 @@ package com.example.letschat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.letschat.databinding.ActivityPhoneNumberBinding;
 
 public class PhoneNumberActivity extends AppCompatActivity {
+
+    ActivityPhoneNumberBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_phone_number);
+        binding=ActivityPhoneNumberBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        getSupportActionBar().hide();
+
+        binding.phoneBox.requestFocus();
+
+        binding.continueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(PhoneNumberActivity.this,OTPActivity.class);
+                intent.putExtra("phoneNumber",binding.phoneBox.getText().toString());
+                startActivity(intent);
+            }
+        });
     }
 }
