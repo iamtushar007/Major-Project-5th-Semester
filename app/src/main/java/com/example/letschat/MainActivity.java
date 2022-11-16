98f75eb6e3f5098a1d97591c95577c9b595575be
iamtushar007 <tushargahtori26@gmail.com>
2022-11-17 04:00:57 +0530

    17th November 2022 :: 4:00 a.m.

--- app/src/main/java/com/example/letschat/MainActivity.java
+++ app/src/main/java/com/example/letschat/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.letschat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.search:
+                Toast.makeText(this,"Searched Clicked",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.settings:
+                Toast.makeText(this,"Settings clicked",Toast.LENGTH_SHORT).show();
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.topmenu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
 }
--- app/src/main/java/com/example/letschat/PhoneNumberActivity.java
+++ app/src/main/java/com/example/letschat/PhoneNumberActivity.java
@@ -7,16 +7,27 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.example.letschat.databinding.ActivityPhoneNumberBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class PhoneNumberActivity extends AppCompatActivity {
 
     ActivityPhoneNumberBinding binding;
+
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding=ActivityPhoneNumberBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        auth=FirebaseAuth.getInstance();
+        if(auth.getCurrentUser()!=null)
+        {
+            Intent intent=new Intent(PhoneNumberActivity.this,MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
 
         getSupportActionBar().hide();
 
