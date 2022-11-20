c96fadabe1b85dec97b176b77d14d971ac0842a8
iamtushar007 <tushargahtori26@gmail.com>
2022-11-20 22:56:52 +0530

    20th November 2022 :: 10:56 a.m.

--- app/src/main/java/com/example/letschat/MainActivity.java
+++ app/src/main/java/com/example/letschat/MainActivity.java
@@ -8,12 +8,51 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.example.letschat.databinding.ActivityMainBinding;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    ActivityMainBinding binding;
+    FirebaseDatabase database;
+    ArrayList <User> users;
+    UsersAdapter usersAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        database=FirebaseDatabase.getInstance();
+        users=new ArrayList<>();
+
+        usersAdapter=new UsersAdapter(this,users);
+        binding.recyclerView.setAdapter(usersAdapter);
+
+        database.getReference().child("users").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                users.clear();
+                for(DataSnapshot snapshot1:snapshot.getChildren()) {
+                     User user=snapshot1.getValue(User.class);
+                     users.add(user);
+                }
+                usersAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
     }
 
 
---
+++ app/src/main/java/com/example/letschat/UsersAdapter.java
@@ -0,0 +1,62 @@
+package com.example.letschat;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.letschat.databinding.RowConversationBinding;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class UsersAdapter extends RecyclerView.Adapter<UsersAdapter.UsersViewHolder> {
+
+
+    Context context;
+    ArrayList<User> users;
+
+    public UsersAdapter(Context context, ArrayList<User> users){
+        this.context=context;
+        this.users=users;
+
+    }
+
+    @NonNull
+    @Override
+    public UsersViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(context).inflate(R.layout.row_conversation,parent,false);
+        return new UsersViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UsersViewHolder holder, int position) {
+        User user=users.get(position);
+
+        holder.binding.username.setText( user.getName());
+
+        Glide.with(context).load(user.getProfileImage())
+                .placeholder(R.drawable.user)
+                .into(holder.binding.profile);
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+
+    public class UsersViewHolder extends RecyclerView.ViewHolder{
+
+        RowConversationBinding binding;
+
+        public UsersViewHolder(@NonNull View itemView) {
+            super(itemView);
+            binding=RowConversationBinding.bind(itemView);
+        }
+    }
+
+}
